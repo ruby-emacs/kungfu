7cfe2d75b332a10f58789c422dddebd5ccdbf4d2
Steve Chan <chanshunli@gmail.com>
2016-10-08 07:40:42 +0800

    add test .e.g

--- kungfu-tests.el
+++ kungfu-tests.el
@@ -8,3 +8,13 @@
    (lambda ()
      [test code])))
 ;;;;
+;; (pp-to-string '(quote quote))          ; expected: "'quote"
+;; (pp-to-string '((quote a) (quote b)))  ; expected: "('a 'b)\n"
+;; (pp-to-string '('a 'b))                ; same as above
+
+(ert-deftest pp-test-quote ()
+  "Tests the rendering of `quote' symbols in `pp-to-string'."
+  (should (equal (pp-to-string '(quote quote)) "'quote"))
+  (should (equal (pp-to-string '((quote a) (quote b))) "('a 'b)\n"))
+  (should (equal (pp-to-string '('a 'b)) "('a 'b)\n")))
+
