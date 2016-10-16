c4e97abc74fd676575e50f4c76c21fe40a6eb6f7
Steve Chan <chanshunli@gmail.com>
2016-10-16 23:36:32 +0800

    add eval test

--- kungfu-tests.el
+++ kungfu-tests.el
@@ -33,16 +33,16 @@
 ;;;; for kungfu.el test ;;;
 
 ;; mock the function
-(defun get-mark-content (buffername) (unwind-protect "Test Abc"))
 (ert-deftest downcase-str ()
+  (defun get-mark-content (buffername) (unwind-protect "Test Abc"))
   (should (equal (downcase-str) "test abc") )) 
 ;; must start drb server for test, add `gem 'drbcli' to Gemfile`
 (ert-deftest rb-underscore ()
   (should (equal (rb-underscore "AdddTaaaGaaa") "addd_taaa_gaaa") ))
 
-(defun relace-region-str (str) str)
-(defun get-point-keyword () "BdasdasYdasds")
 (ert-deftest rb-underscore-words ()
+  (defun relace-region-str (str) str)
+  (defun get-point-keyword () "BdasdasYdasds")
   (should (equal (rb-underscore-words) "bdasdas_ydasds") ))
 
 (ert-deftest rb-camelize ()
@@ -108,5 +108,16 @@
   (should (equal find-line 4))
   (makunbound 'find-buffer)
   (makunbound 'find-line)
+  )
+
+(ert-deftest rb-eval ()
+  (rb-eval "1+1")
+  (should (equal (get-messages-last-line) "2"))
+  )
+
+(ert-deftest rb-eval-var ()
+  (rb-eval "def aa;11;end")
+  (defun get-point-keyword () "aa")
+  (should (equal (rb-eval-var) "11"))
   )
 
